d1fd19d18cb1b3e3992528e1fce33bfc3437978f
cnorton27 <ciaranorton@hotmail.com>
2024-03-04 12:31:09 -0800

    data cleaning, added ID column

--- parsing_feature_col.py
+++ parsing_feature_col.py
@@ -13,19 +13,21 @@ geojson_files = glob.glob(os.path.join(directory, '*.geojson'))
 
 # Create an empty list to store GeoDataFrames
 gdfs = []
-
 # Iterate over each GeoJSON file and load it into GeoDataFrame
 for file in geojson_files:
     # Read GeoJSON file
     feature_collection_name = os.path.splitext(os.path.basename(file))[0]
+    ID= 0
+    
     gdf = gpd.read_file(file)
-
+    gdf["ID"] = ID
     gdf['feature_collection_name'] = feature_collection_name
     print(feature_collection_name)
-    
 
     # Append GeoDataFrame to list
     gdfs.append(gdf)
+
+    
 
 # Concatenate all GeoDataFrames into a single GeoDataFrame
 combined_gdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
@@ -35,9 +37,26 @@ combined_gdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
 print(combined_gdf.head())
 
 # Specify the output GeoJSON file path
-output_geojson_file = 'output_noroads.geojson'
+output_geojson_file = 'trial.geojson'
 
 # Export GeoDataFrame to GeoJSON
 combined_gdf.to_file(output_geojson_file, driver='GeoJSON')
 
 print("GeoDataFrame successfully exported to GeoJSON:", output_geojson_file)
+
+
+
+# Read the GeoJSON file into a GeoDataFrame
+gdf = gpd.read_file(r'C:\Users\ciara\OneDrive\Documents\GitHub\Exploratory-Lab-1\trial.geojson')
+
+count = 1
+
+# Iterate over every feature in the GeoDataFrame
+for index, row in gdf.iterrows():
+    count = count + 1
+    # Access and modify the value of a specific property using .loc
+    gdf.loc[index, 'ID'] = count
+    print(gdf.loc[index, 'ID'])
+
+# Save the modified GeoDataFrame to a new GeoJSON file
+gdf.to_file('withID.geojson', driver='GeoJSON')
