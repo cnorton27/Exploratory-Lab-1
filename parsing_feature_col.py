07283e4540ae256c6fabbfb6e8b20505cb032752
cnorton27 <ciaranorton@hotmail.com>
2024-03-08 12:28:54 -0800

    file cleaning

--- parsing_feature_col.py
+++ parsing_feature_col.py
@@ -28,35 +28,27 @@ for file in geojson_files:
     gdfs.append(gdf)
 
     
-
-# Concatenate all GeoDataFrames into a single GeoDataFrame
 combined_gdf = gpd.GeoDataFrame(pd.concat(gdfs, ignore_index=True))
 
-# Now combined_gdf contains all the data from all GeoJSON files in the directory,
-# and it has a new column 'feature_collection_name' storing the name of each feature collection
+
 print(combined_gdf.head())
 
-# Specify the output GeoJSON file path
 output_geojson_file = 'trial.geojson'
 
-# Export GeoDataFrame to GeoJSON
+# Export to JSON
 combined_gdf.to_file(output_geojson_file, driver='GeoJSON')
-
-print("GeoDataFrame successfully exported to GeoJSON:", output_geojson_file)
-
-
 
 # Read the GeoJSON file into a GeoDataFrame
 gdf = gpd.read_file(r'C:\Users\ciara\OneDrive\Documents\GitHub\Exploratory-Lab-1\trial.geojson')
 
 count = 1
 
-# Iterate over every feature in the GeoDataFrame
+# Adding ID column using count
 for index, row in gdf.iterrows():
     count = count + 1
-    # Access and modify the value of a specific property using .loc
+    # Access and modify the value new ID property using .loc
     gdf.loc[index, 'ID'] = count
     print(gdf.loc[index, 'ID'])
 
-# Save the modified GeoDataFrame to a new GeoJSON file
+# Save as a new GeoJSON file
 gdf.to_file('withID.geojson', driver='GeoJSON')
